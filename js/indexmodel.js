a4d9afcd723ec5e527c6c95e112b70db378f43dd
Victory Chibueze Akunna <victorychibuakunna@gmail.com>
2025-02-04 07:48:41 +0000

    deloying update to git

---
+++ js/indexmodel.js
@@ -0,0 +1,201 @@
+'use strict';
+
+import { output } from "./indexview.js";
+import { outputint } from "./indexview.js";
+import { outputsimult } from "./indexview.js";
+
+
+///////////////////////////////
+class Linear_diff {
+    constructor() {
+        //calling the initials to access the Dom Elements
+        this.power = power;
+        this.coef_ = coef_;
+        this.variable = variable;
+        this.power_values;
+        this.coef_values;
+        this.differential;
+        this.variable_value;
+        this.second_power;
+        this.new_coef_;
+        this.equation;
+        this.equations = [];        
+    }
+ //////////////////////////////
+ //class prototype that helps in linear differentiation
+    _diff(){
+        this.power_values = Number(this.power.value);
+        this.coef_values = Number(this.coef_.value);
+        this.variable_value = this.variable.value;
+        this.equations = [];
+    
+        this.second_power = 1;
+        this.new_coef_ = this.coef_values * this.second_power;
+        this.differential = this.power_values;
+        while (this.power_values > 0 || (this.power_values < 0 && this.power_values > -10)) {
+            this.new_coef_ = this.new_coef_ * this.second_power
+            this.equation = `${Math.abs(this.power_values - this.differential)}. differential ${this.new_coef_}${this.variable_value}<sup>${this.power_values}</sup> <br>`;
+            this.equations.push(this.equation);
+            this.second_power = this.power_values;
+            this.power_values--;
+            if (this.power_values === 0) {
+                this.equation = `${Math.abs(this.power_values - this.differential)}. differential ${this.new_coef_} <br>`;
+                this.equations.push(this.equation);
+            }
+            
+        };
+        const textStore = [];
+        let i = 0;
+        function animate(text){
+            if (i < text.length) {
+                let show = text.split('')[i];
+                textStore.push(show);
+                i++ 
+                return textStore.join('');
+            }
+            else{
+                clearInterval(interval)
+            }
+            return textStore.join('');
+        }
+        const interval = setInterval(() => {
+            output.innerHTML = animate(this.equations.join(''))
+        }, 50);
+    
+
+       // output.innerHTML = this.equations.join('');  
+        this.power.value = '';
+        this.coef_.value = '';
+        this.variable.value = '';
+    };
+
+}
+
+////////////////////////
+
+export const linear_diff = new Linear_diff();
+
+
+
+//////////////////////////////////
+//class integral
+class Linear_int {
+    constructor() {
+        //calling the initials to access the Dom Elements
+        this.powerint = powerint;
+        this.coefint_ = coefint_;
+        this.variableint = variableint;
+        this.powerint_values;
+        this.coefint_values;
+        this.variableint_value;
+        //this.equation;
+        this.equations = [];        
+    }
+ //////////////////////////////
+ //class prototype that helps in linear integration
+    _int(){
+        this.powerint_values = Number(this.powerint.value);
+        this.coefint_values = Number(this.coefint_.value);
+        this.variableint_value = this.variableint.value;
+        this.equations = [];
+
+
+        this.equation = `${this.coefint_values}${this.variableint_value}<sup>${this.powerint_values} </sup> <br>`;
+        this.equations.push(this.equation);
+        this.equation = `integral: (${this.coefint_values}${this.variableint_value}<sup>${this.powerint_values + 1}</sup>)
+         / ${this.powerint_values + 1} + C <br>`;
+        this.equations.push(this.equation);
+        const textStore = [];
+        let i = 0;
+        function animate(text){
+            if (i < text.length) {
+                let show = text.split('')[i];
+                textStore.push(show);
+                i++ 
+                return textStore.join('');
+            }
+            else{
+                clearInterval(interval)
+            }
+            return textStore.join('');
+        }
+        const interval = setInterval(() => {
+            outputint.innerHTML = animate(this.equations.join(''))
+        }, 50);  
+        this.powerint.value = '';
+        this.coefint_.value = '';
+        this.variableint.value = '';
+    };
+/////////////////////////////////////////////////
+}
+
+////////////////////////
+
+export const linear_int = new Linear_int();
+
+
+
+
+
+
+
+
+
+/////////////////////////////////////////
+//class simulteneous equation//////////////////////////////////
+//class integral
+class Simult {
+    constructor() {
+        //calling the initials to access the Dom Elements
+        this.a = a;
+        this.b = b;
+        this.c = c;
+        this.d = d;
+        this.e = e;
+        this.f = f;
+        this.a_values;
+        this.b_values;
+        this.c_values;
+        this.d_values;
+        this.e_values;
+        this.f_values;
+        //this.equation;
+        this.x;
+        this.y;
+               
+    }
+ //////////////////////////////
+ //class prototype that helps in linear integration
+    _simult(){
+        this.a_values = Number(this.a.value);
+        this.b_values = Number(this.b.value);
+        this.c_values = Number(this.c.value);
+        this.d_values = Number(this.d.value);
+        this.e_values = Number(this.e.value);
+        this.f_values = Number(this.f.value);
+        let firstx = this.c_values*((this.e_values * this.a_values )- (this.b_values * this.d_values)) + this.b_values*((this.d_values * this.c_values) - (this.f_values * this.a_values));
+        let secondx = ((this.e_values*this.a_values*this.a_values) - (this.a_values*this.b_values*this.d_values));
+        this.x = firstx / secondx;
+        let firsty = ((this.f_values*this.a_values) - (this.d_values*this.c_values));
+        let secondy = ((this.e_values * this.a_values)-(this.b_values*this.d_values));
+        this.y = firsty / secondy;
+        if (secondy === 0 || secondx === 0) {
+            outputsimult.innerHTML = ''; 
+        }else{
+            outputsimult.innerHTML = `X = ${this.x} <br> Y = ${this.y}`;
+        }
+
+        this.a.value = '';
+        this.b.value = '';
+        this.c.value = '';
+        this.d.value = '';
+        this.e.value = '';
+        this.f.value =''
+
+    }
+/////////////////////////////////////////////////
+}
+
+////////////////////////
+
+export const simult  = new Simult();
--- js/indexview.js
+++ js/indexview.js
@@ -1,64 +1,152 @@
 'use strict';
-const button = document.querySelector('.button');
-const element = document.querySelector('.element');
-const output = document.querySelector('.output');
+import { linear_diff } from "./indexmodel.js"
+import { linear_int } from "./indexmodel.js"
+import { simult } from "./indexmodel.js";
+
+
 const submit_diff = document.querySelector('.submit_diff');
-
-
-///////////////////////////////
-class Linear_diff {
-    constructor() {
-        //calling the initials to access the Dom Elements
-        this.power = document.querySelector('#power');
-        this.coef_ = document.querySelector('#coef_');
-        this.variable = document.querySelector('#variable');
-        this.power_values;
-        this.coef_values;
-        this.variable_value;
-        this.second_power;
-        this.new_coef_;
-        this.equation;
-        this.equations = [];        
-    }
- //////////////////////////////
-    _diff(){
-        this.power_values = Number(this.power.value);
-        this.coef_values = Number(this.coef_.value);
-        this.variable_value = this.variable.value;
-
-        output.innerHTML = 'yyyyyy';
-    
-        this.second_power = 1;
-        this.new_coef_ = this.coef_values * this.second_power;
-        while (this.power_values > 0) {
-            this.new_coef_ = this.new_coef_ * this.second_power
-            this.equation = `${this.new_coef_}${this.variable_value}<sup>${this.power_values}</sup>`;
-            this.equations.push(this.equation);
-            this.second_power = this.power_values;
-            this.power_values--;
-            if (this.power_values === 0) {
-                this.equation = this.new_coef_;
-                this.equations.push(this.equation);
-            }
-        };
-
-        output.innerHTML = this.equations.join(' >>> ');  
-        this.power.value = '';
-        this.coef_.value = '';
-        this.variable.value = '';
-    };
-
-
-}
-
-////////////////////////
-
-const linear_diff = new Linear_diff();
-
+const scrolldown= document.querySelector('.scrolldown');
+const submit_int = document.querySelector('.submit_int');
+const Linear_int = document.querySelector('.Linear_int');
+const submit_simult = document.querySelector('.submit_simult');
+const title1 = document.querySelector('.title1');
+const title2 = document.querySelector('.title2')
 
 
  ///////////////////////////////////
- ////submiting the solving
+ ////submiting the solving of differentiation
 submit_diff.addEventListener('click', function() {
     linear_diff._diff()
 })
+
+ ///////////////////////////////////
+ ////submiting the solving of integration
+ submit_int.addEventListener('click', function() {
+    linear_int._int()
+})
+
+
+ ///////////////////////////////////
+ ////submiting the solving of simulteneous equation
+ submit_simult.addEventListener('click', function(e) {
+    e.preventDefault();
+    simult._simult()
+})
+
+
+
+//////////////////////
+//output for differentiation
+export const output = document.querySelector('.output');
+
+
+//////////////////////
+//output for integration
+export const outputint = document.querySelector('.outputint');
+
+//////////////////////
+//output for simulteneous equation
+export const outputsimult = document.querySelector('.outputsimult');
+
+//////////////////////////////////////
+//displaying the table
+const open = document.querySelector('.open');
+const close = document.querySelector('.close');
+const table = document.querySelector('.table');
+
+
+class display{
+    constructor(){
+        this._toggle();
+        this._scrolldown();
+        this._textAnimation()
+    }
+//////////////////////////////////////////////////////////////
+        _toggle(){
+            function events() {
+                open.classList.remove(this === open ? 'show' : 'noshow');
+                 open.classList.add(this === open ? 'noshow' : 'show');
+                 close.classList.remove(this === open ? 'noshow' : 'show');
+                 close.classList.add(this === open ? 'show' : 'noshow');
+                 table.classList.remove(this === open ? 'noshow' : 'show');
+                 table.classList.add(this === open ? 'show' : 'noshow')
+             };
+             open.addEventListener('click', events)
+             close.addEventListener('click', events)
+        }
+////////////////////////////////////////////////////////////
+        _scrolldown(){
+            scrolldown.style.top = `${window.innerHeight - 50}px`;
+            scrolldown.style.left = `${window.innerWidth/2}px`;
+            scrolldown.addEventListener('click', function(){
+                Linear_int.scrollIntoView({behavior: "smooth"})
+            });
+
+
+            ///////////////////////////////
+        function obsfunction(entries){
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    Linear_int.style.opacity = '100%';
+                    Linear_int.style.marginTop = '10px';
+                    scrolldown.style.visibility = 'hidden'
+                    const textStore = [];
+            let i = 0;
+            //Animating the title
+            function animate(text){
+                if (i < text.length) {
+                    let show = text.split('')[i];
+                    textStore.push(show);
+                    i++ 
+                    return textStore.join('');
+                }
+                else{
+                    clearInterval(interval)
+                }
+                return textStore.join('');
+            }
+        const interval = setInterval(() => {
+            title2.innerHTML = animate('INTEGRATION(Linear)  coef_ X <sup>power</sup>')
+        }, 100);
+
+
+               }else{
+                    Linear_int.style.opacity = '50%';
+                    Linear_int.style.marginTop = '200px';   
+                    scrolldown.style.visibility = 'visible';
+                    title2.innerHTML = ''         
+               }
+            });
+            }
+            const obsoption = {
+                thresholds: 1.0,
+                roots:null
+            }
+            const observer = new IntersectionObserver(obsfunction,obsoption);
+            observer.observe(Linear_int)
+        
+        }
+///////////////////////////////////////////////////////////
+        _textAnimation(){
+            const textStore = [];
+            let i = 0;
+            function animate(text){
+                if (i < text.length) {
+                    let show = text.split('')[i];
+                    textStore.push(show);
+                    i++ 
+                    return textStore.join('');
+                }
+                else{
+                    clearInterval(interval)
+                }
+                return textStore.join('');
+            }
+        const interval = setInterval(() => {
+            title1.innerHTML = animate('DIFFERENTIATION(Linear)  coef_ X <sup>power</sup>')
+        }, 100);
+        }
+
+ }
+
+const Display = new display()
